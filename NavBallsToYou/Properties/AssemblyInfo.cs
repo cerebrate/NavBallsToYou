5735204ac46d22feca9e024fc4eb90839c35075b
Alistair Young <avatar@arkane-systems.net>
2016-10-12 21:12:03 -0500

    Recompile against KSP 1.2

--- NavBallsToYou/Properties/AssemblyInfo.cs
+++ NavBallsToYou/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NavBallsToYou")]
-[assembly: AssemblyDescription("KSP 1.1 tweak which makes the navball appear automagically in map view.")]
+[assembly: AssemblyDescription("KSP 1.2 tweak which makes the navball appear automagically in map view.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arkane Systems")]
 [assembly: AssemblyProduct("NavBallsToYou")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
